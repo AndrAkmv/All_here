422c71ed089faa12fca524ca379114e18c835503
Andrej Akimov <andrakmv@gmail.com>
2018-05-21 14:28:53 +0300

    Approved updates for Office 2016

--- _5 Microsoft Updates/Office 2016 - Updates/Extract-2016.cmd
+++ _5 Microsoft Updates/Office 2016 - Updates/Extract-2016.cmd
@@ -17,9 +17,6 @@ set /a _num+=1
 echo %_num% Extract: exppdf2016-kb2920720-fullfile-x86-glb.exe
 start /w x86\exppdf2016-kb2920720-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: osetup2016-kb2920723-fullfile-x86-glb.exe
-start /w x86\osetup2016-kb2920723-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: ospp2016-kb2920724-fullfile-x86-glb.exe
 start /w x86\ospp2016-kb2920724-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
@@ -44,9 +41,6 @@ set /a _num+=1
 echo %_num% Extract: gkall2016-kb3115276-fullfile-x86-glb.exe
 start /w x86\gkall2016-kb3115276-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: stslist2016-kb3115281-fullfile-x86-glb.exe
-start /w x86\stslist2016-kb3115281-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: outlfltr2016-kb3115407-fullfile-x86-glb.exe
 start /w x86\outlfltr2016-kb3115407-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
@@ -65,8 +59,8 @@ set /a _num+=1
 echo %_num% Extract: outexum2016-kb3141506-fullfile-x86-glb.exe
 start /w x86\outexum2016-kb3141506-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: proof2016-kb3141509-fullfile-x86-glb.exe
-start /w x86\proof2016-kb3141509-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: proof2016-kb3178662-fullfile-x86-glb.exe
+start /w x86\proof2016-kb3178662-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: msmipc2016-kb3178666-fullfile-x86-glb.exe
 start /w x86\msmipc2016-kb3178666-fullfile-x86-glb.exe /extract:x86-msp /quiet
@@ -77,17 +71,11 @@ set /a _num+=1
 echo %_num% Extract: groove2016-kb3178707-fullfile-x86-glb.exe
 start /w x86\groove2016-kb3178707-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: osfclient2016-kb3191923-fullfile-x86-glb.exe
-start /w x86\osfclient2016-kb3191923-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: ace2016-kb3191924-fullfile-x86-glb.exe
-start /w x86\ace2016-kb3191924-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: kohhc2016-kb3191929-fullfile-x86-glb.exe
 start /w x86\kohhc2016-kb3191929-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: msodll202016-kb3213542-fullfile-x86-glb.exe
-start /w x86\msodll202016-kb3213542-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: stslist2016-kb3203479-fullfile-x86-glb.exe
+start /w x86\stslist2016-kb3203479-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: conv2016-kb3213551-fullfile-x86-glb.exe
 start /w x86\conv2016-kb3213551-fullfile-x86-glb.exe /extract:x86-msp /quiet
@@ -95,71 +83,86 @@ set /a _num+=1
 echo %_num% Extract: xdext2016-kb3213650-fullfile-x86-glb.exe
 start /w x86\xdext2016-kb3213650-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: csi2016-kb4011031-fullfile-x86-glb.exe
-start /w x86\csi2016-kb4011031-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: riched202016-kb4011036-fullfile-x86-glb.exe
-start /w x86\riched202016-kb4011036-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: chart2016-kb4011095-fullfile-x86-glb.exe
-start /w x86\chart2016-kb4011095-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: visio2016-kb4011136-fullfile-x86-glb.exe
-start /w x86\visio2016-kb4011136-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: onenote2016-kb4011137-fullfile-x86-glb.exe
-start /w x86\onenote2016-kb4011137-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: access2016-kb4011142-fullfile-x86-glb.exe
-start /w x86\access2016-kb4011142-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: msodll99l2016-kb4011163-fullfile-x86-glb.exe
-start /w x86\msodll99l2016-kb4011163-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: ose2016-kb4011185-fullfile-x86-glb.exe
-start /w x86\ose2016-kb4011185-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: graph2016-kb4011211-fullfile-x86-glb.exe
-start /w x86\graph2016-kb4011211-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: ace2016-kb4011143-fullfile-x86-glb.exe
+start /w x86\ace2016-kb4011143-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: excelpp2016-kb4011218-fullfile-x86-glb.exe
 start /w x86\excelpp2016-kb4011218-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: powerpoint2016-kb4011219-fullfile-x86-glb.exe
-start /w x86\powerpoint2016-kb4011219-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: ppaddin2016-kb4011225-fullfile-x86-glb.exe
 start /w x86\ppaddin2016-kb4011225-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: ose2016-kb4011237-fullfile-x86-glb.exe
+start /w x86\ose2016-kb4011237-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: osetup2016-kb4011239-fullfile-x86-glb.exe
+start /w x86\osetup2016-kb4011239-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: acewss2016-kb4011259-fullfile-x86-glb.exe
 start /w x86\acewss2016-kb4011259-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: eqnedt322016-kb4011262-fullfile-x86-glb.exe
-start /w x86\eqnedt322016-kb4011262-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: oart2016-kb4011562-fullfile-x86-glb.exe
-start /w x86\oart2016-kb4011562-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: lync2016-kb4011563-fullfile-x86-glb.exe
-start /w x86\lync2016-kb4011563-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: mso2016-kb4011567-fullfile-x86-glb.exe
 start /w x86\mso2016-kb4011567-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: msodll302016-kb4011568-fullfile-x86-glb.exe
-start /w x86\msodll302016-kb4011568-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: riched202016-kb4011569-fullfile-x86-glb.exe
+start /w x86\riched202016-kb4011569-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: outlook2016-kb4011570-fullfile-x86-glb.exe
-start /w x86\outlook2016-kb4011570-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: eqnedt322016-kb4011574-fullfile-x86-glb.exe
+start /w x86\eqnedt322016-kb4011574-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: msodll40ui2016-kb4011572-fullfile-x86-glb.exe
-start /w x86\msodll40ui2016-kb4011572-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: oart2016-kb4011628-fullfile-x86-glb.exe
+start /w x86\oart2016-kb4011628-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: project2016-kb4011573-fullfile-x86-glb.exe
-start /w x86\project2016-kb4011573-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: msodll202016-kb4011630-fullfile-x86-glb.exe
+start /w x86\msodll202016-kb4011630-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: word2016-kb4011575-fullfile-x86-glb.exe
-start /w x86\word2016-kb4011575-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: csi2016-kb4011634-fullfile-x86-glb.exe
+start /w x86\csi2016-kb4011634-fullfile-x86-glb /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: excel2016-kb4011577-fullfile-x86-glb.exe
-start /w x86\excel2016-kb4011577-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: visio2016-kb4011661-fullfile-x86-glb.exe
+start /w x86\visio2016-kb4011661-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: access2016-kb4011665-fullfile-x86-glb.exe
+start /w x86\access2016-kb4011665-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: msodll302016-kb4011667-fullfile-x86-glb.exe
+start /w x86\msodll302016-kb4011667-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: msodll40ui2016-kb4011671-fullfile-x86-glb.exe
+start /w x86\msodll40ui2016-kb4011671-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: powerpoint2016-kb4011726-fullfile-x86-glb.exe
+start /w x86\powerpoint2016-kb4011726-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: msodll99l2016-kb4018318-fullfile-x86-glb.exe
+start /w x86\msodll99l2016-kb4018318-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: chart2016-kb4018319-fullfile-x86-glb.exe
+start /w x86\chart2016-kb4018319-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: onenote2016-kb4018321-fullfile-x86-glb.exe
+start /w x86\onenote2016-kb4018321-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: graph2016-kb4018327-fullfile-x86-glb.exe
+start /w x86\graph2016-kb4018327-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: lync2016-kb4018367-fullfile-x86-glb.exe
+start /w x86\lync2016-kb4018367-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: osfclient2016-kb4018369-fullfile-x86-glb.exe
+start /w x86\osfclient2016-kb4018369-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: outlook2016-kb4018372-fullfile-x86-glb.exe
+start /w x86\outlook2016-kb4018372-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: project2016-kb4018373-fullfile-x86-glb.exe
+start /w x86\project2016-kb4018373-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: excel2016-kb4018382-fullfile-x86-glb.exe
+start /w x86\excel2016-kb4018382-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: word2016-kb4018383-fullfile-x86-glb.exe
+start /w x86\word2016-kb4018383-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: msohevi2016-kb4022133-fullfile-x86-glb.exe
+start /w x86\msohevi2016-kb4022133-fullfile-x86-glb.exe /extract:x86-msp /quiet
