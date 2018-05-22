fde5a134c2ae13379b98327630776de92c2972cc
Andrej Akimov <andrakmv@gmail.com>
2018-05-22 21:16:06 +0300

    Approved updates for Office 2013

--- _5 Microsoft Updates/Office 2013 - Updates/Extract-2013.cmd
+++ _5 Microsoft Updates/Office 2013 - Updates/Extract-2013.cmd
@@ -1,5 +1,5 @@
 @echo off
-echo Extracting updates for Microsoft Office 2013 Standard SP1 (December 2017)
+echo Extracting updates for Microsoft Office 2013 Standard SP1 (May 2018)
 echo -------------------------------------------------------------------------------
 set /a _num+=1
 echo %_num% Extract: ocr2013-kb2760344-fullfile-x86-glb.exe
@@ -35,9 +35,6 @@ set /a _num+=1
 echo %_num% Extract: otkruntimertl2013-kb3039746-fullfile-x86-glb.exe
 start /w x86\otkruntimertl2013-kb3039746-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: ace2013-kb3039750-fullfile-x86-glb.exe
-start /w x86\ace2013-kb3039750-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: filterpack2013-kb3039756-fullfile-x86-glb.exe
 start /w x86\filterpack2013-kb3039756-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
@@ -55,9 +52,6 @@ start /w x86\gkall2013-kb3039795-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: ieawsdc2013-kb3039798-fullfile-x86-glb.exe
 start /w x86\ieawsdc2013-kb3039798-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: msptls2013-kb3054816-fullfile-x86-glb.exe
-start /w x86\msptls2013-kb3054816-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: analys322013-kb3054819-fullfile-x86-glb.exe
 start /w x86\analys322013-kb3054819-fullfile-x86-glb.exe /extract:x86-msp /quiet
@@ -92,17 +86,8 @@ set /a _num+=1
 echo %_num% Extract: wxpcore2013-kb3114499-fullfile-x86-glb.exe
 start /w x86\wxpcore2013-kb3114499-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: ipeditor2013-kb3114818-fullfile-x86-glb.exe
-start /w x86\ipeditor2013-kb3114818-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: riched202013-kb3114825-fullfile-x86-glb.exe
-start /w x86\riched202013-kb3114825-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: infopath2013-kb3114946-fullfile-x86-glb.exe
 start /w x86\infopath2013-kb3114946-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: proofloc2013-kb3115156-fullfile-x86-glb.exe
-start /w x86\proofloc2013-kb3115156-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: outlfltr2013-kb3115404-fullfile-x86-glb.exe
 start /w x86\outlfltr2013-kb3115404-fullfile-x86-glb.exe /extract:x86-msp /quiet
@@ -110,32 +95,41 @@ set /a _num+=1
 echo %_num% Extract: infopathpc2013-kb3127916-fullfile-x86-glb.exe
 start /w x86\infopathpc2013-kb3127916-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: eqnedt322013-kb3162047-fullfile-x86-glb.exe
-start /w x86\eqnedt322013-kb3162047-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: dcf2013-kb3162051-fullfile-x86-glb.exe
 start /w x86\dcf2013-kb3162051-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: ipeditor2013-kb3162075-fullfile-x86-glb.exe
+start /w x86\ipeditor2013-kb3162075-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: wordpia2013-kb3162081-fullfile-x86-glb.exe
 start /w x86\wordpia2013-kb3162081-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
+echo %_num% Extract: graph2013-kb3172436-fullfile-x86-glb.exe
+start /w x86\graph2013-kb3172436-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
 echo %_num% Extract: vbe72013-kb3172443-fullfile-x86-glb.exe
 start /w x86\vbe72013-kb3172443-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
+echo %_num% Extract: ace2013-kb3172459-fullfile-x86-glb.exe
+start /w x86\ace2013-kb3172459-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: riched202013-kb3172471-fullfile-x86-glb.exe
+start /w x86\riched202013-kb3172471-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: proofloc2013-kb3172510-fullfile-x86-glb.exe
+start /w x86\proofloc2013-kb3172510-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
 echo %_num% Extract: msmipc2013-kb3172523-fullfile-x86-glb.exe
 start /w x86\msmipc2013-kb3172523-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: ose2013-kb3172524-fullfile-x86-glb.exe
-start /w x86\ose2013-kb3172524-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
-echo %_num% Extract: osetup2013-kb3172531-fullfile-x86-glb.exe
-start /w x86\osetup2013-kb3172531-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: wec2013-kb3172533-fullfile-x86-glb.exe
 start /w x86\wec2013-kb3172533-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: csi2013-kb3172545-fullfile-x86-glb.exe
 start /w x86\csi2013-kb3172545-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: msptls2013-kb3178636-fullfile-x86-glb.exe
+start /w x86\msptls2013-kb3178636-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
 echo %_num% Extract: groove2013-kb3178645-fullfile-x86-glb.exe
 start /w x86\groove2013-kb3178645-fullfile-x86-glb.exe /extract:x86-msp /quiet
@@ -149,35 +143,44 @@ set /a _num+=1
 echo %_num% Extract: conv2013-kb3213564-fullfile-x86-glb.exe
 start /w x86\conv2013-kb3213564-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: onenote2013-kb4011075-fullfile-x86-glb.exe
-start /w x86\onenote2013-kb4011075-fullfile-x86-glb.exe /extract:x86-msp /quiet
-set /a _num+=1
 echo %_num% Extract: seguiemj2013-kb4011087-fullfile-x86-glb.exe
 start /w x86\seguiemj2013-kb4011087-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: osfclient2013-kb4011106-fullfile-x86-glb.exe
-start /w x86\osfclient2013-kb4011106-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: ose2013-kb4011253-fullfile-x86-glb.exe
+start /w x86\ose2013-kb4011253-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: oart2013-kb4011277-fullfile-x86-glb.exe
-start /w x86\oart2013-kb4011277-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: osetup2013-kb4011254-fullfile-x86-glb.exe
+start /w x86\osetup2013-kb4011254-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: powerpoint2013-kb4011278-fullfile-x86-glb.exe
-start /w x86\powerpoint2013-kb4011278-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: onenote2013-kb4011281-fullfile-x86-glb.exe
+start /w x86\onenote2013-kb4011281-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: mso2013-kb4011279-fullfile-x86-glb.exe
-start /w x86\mso2013-kb4011279-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: eqnedt322013-kb4011580-fullfile-x86-glb.exe
+start /w x86\eqnedt322013-kb4011580-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: outlook2013-kb4011282-fullfile-x86-glb.exe
-start /w x86\outlook2013-kb4011282-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: oart2013-kb4018288-fullfile-x86-glb.exe
+start /w x86\oart2013-kb4018288-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: lync2013-kb4011284-fullfile-x86-glb.exe
-start /w x86\lync2013-kb4011284-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: powerpoint2013-kb4018289-fullfile-x86-glb.exe
+start /w x86\powerpoint2013-kb4018289-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: project2013-kb4011285-fullfile-x86-glb.exe
-start /w x86\project2013-kb4011285-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: osfclient2013-kb4018333-fullfile-x86-glb.exe
+start /w x86\osfclient2013-kb4018333-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: word2013-kb4011590-fullfile-x86-glb.exe
-start /w x86\word2013-kb4011590-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: outlook2013-kb4018376-fullfile-x86-glb.exe
+start /w x86\outlook2013-kb4018376-fullfile-x86-glb.exe /extract:x86-msp /quiet
 set /a _num+=1
-echo %_num% Extract: excel2013-kb4011597-fullfile-x86-glb.exe
-start /w x86\excel2013-kb4011597-fullfile-x86-glb.exe /extract:x86-msp /quiet
+echo %_num% Extract: lync2013-kb4018377-fullfile-x86-glb.exe
+start /w x86\lync2013-kb4018377-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: project2013-kb4018379-fullfile-x86-glb.exe
+start /w x86\project2013-kb4018379-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: mso2013-kb4018389-fullfile-x86-glb.exe
+start /w x86\mso2013-kb4018389-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: word2013-kb4018396-fullfile-x86-glb.exe
+start /w x86\word2013-kb4018396-fullfile-x86-glb.exe /extract:x86-msp /quiet
+set /a _num+=1
+echo %_num% Extract: excel2013-kb4018399-fullfile-x86-glb.exe
+start /w x86\excel2013-kb4018399-fullfile-x86-glb.exe /extract:x86-msp /quiet
--- _5 Microsoft Updates/Office 2013 - Updates/Install-2013.cmd
+++ _5 Microsoft Updates/Office 2013 - Updates/Install-2013.cmd
@@ -1,5 +1,5 @@
 @echo off
-echo Installing updates for Microsoft Office 2013 Standard SP1 (December 2017)
+echo Installing updates for Microsoft Office 2013 Standard SP1 (May 2018)
 echo -------------------------------------------------------------------------------
 set /a _num+=1
 echo %_num% Install: ocr2013-kb2760344-fullfile-x86-glb.exe
@@ -35,9 +35,6 @@ set /a _num+=1
 echo %_num% Install: otkruntimertl2013-kb3039746-fullfile-x86-glb.exe
 start /w x86\otkruntimertl2013-kb3039746-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: ace2013-kb3039750-fullfile-x86-glb.exe
-start /w x86\ace2013-kb3039750-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
 echo %_num% Install: filterpack2013-kb3039756-fullfile-x86-glb.exe
 start /w x86\filterpack2013-kb3039756-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
@@ -55,9 +52,6 @@ start /w x86\gkall2013-kb3039795-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: ieawsdc2013-kb3039798-fullfile-x86-glb.exe
 start /w x86\ieawsdc2013-kb3039798-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
-echo %_num% Install: msptls2013-kb3054816-fullfile-x86-glb.exe
-start /w x86\msptls2013-kb3054816-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: analys322013-kb3054819-fullfile-x86-glb.exe
 start /w x86\analys322013-kb3054819-fullfile-x86-glb.exe /quiet /passive
@@ -92,17 +86,8 @@ set /a _num+=1
 echo %_num% Install: wxpcore2013-kb3114499-fullfile-x86-glb.exe
 start /w x86\wxpcore2013-kb3114499-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: ipeditor2013-kb3114818-fullfile-x86-glb.exe
-start /w x86\ipeditor2013-kb3114818-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
-echo %_num% Install: riched202013-kb3114825-fullfile-x86-glb.exe
-start /w x86\riched202013-kb3114825-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
 echo %_num% Install: infopath2013-kb3114946-fullfile-x86-glb.exe
 start /w x86\infopath2013-kb3114946-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
-echo %_num% Install: proofloc2013-kb3115156-fullfile-x86-glb.exe
-start /w x86\proofloc2013-kb3115156-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: outlfltr2013-kb3115404-fullfile-x86-glb.exe
 start /w x86\outlfltr2013-kb3115404-fullfile-x86-glb.exe /quiet /passive
@@ -110,32 +95,41 @@ set /a _num+=1
 echo %_num% Install: infopathpc2013-kb3127916-fullfile-x86-glb.exe
 start /w x86\infopathpc2013-kb3127916-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: eqnedt322013-kb3162047-fullfile-x86-glb.exe
-start /w x86\eqnedt322013-kb3162047-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
 echo %_num% Install: dcf2013-kb3162051-fullfile-x86-glb.exe
 start /w x86\dcf2013-kb3162051-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: ipeditor2013-kb3162075-fullfile-x86-glb.exe
+start /w x86\ipeditor2013-kb3162075-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: wordpia2013-kb3162081-fullfile-x86-glb.exe
 start /w x86\wordpia2013-kb3162081-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
+echo %_num% Install: graph2013-kb3172436-fullfile-x86-glb.exe
+start /w x86\graph2013-kb3172436-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
 echo %_num% Install: vbe72013-kb3172443-fullfile-x86-glb.exe
 start /w x86\vbe72013-kb3172443-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
+echo %_num% Install: ace2013-kb3172459-fullfile-x86-glb.exe
+start /w x86\ace2013-kb3172459-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: riched202013-kb3172471-fullfile-x86-glb.exe
+start /w x86\riched202013-kb3172471-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: proofloc2013-kb3172510-fullfile-x86-glb.exe
+start /w x86\proofloc2013-kb3172510-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
 echo %_num% Install: msmipc2013-kb3172523-fullfile-x86-glb.exe
 start /w x86\msmipc2013-kb3172523-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
-echo %_num% Install: ose2013-kb3172524-fullfile-x86-glb.exe
-start /w x86\ose2013-kb3172524-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
-echo %_num% Install: osetup2013-kb3172531-fullfile-x86-glb.exe
-start /w x86\osetup2013-kb3172531-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: wec2013-kb3172533-fullfile-x86-glb.exe
 start /w x86\wec2013-kb3172533-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: csi2013-kb3172545-fullfile-x86-glb.exe
 start /w x86\csi2013-kb3172545-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: msptls2013-kb3178636-fullfile-x86-glb.exe
+start /w x86\msptls2013-kb3178636-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
 echo %_num% Install: groove2013-kb3178645-fullfile-x86-glb.exe
 start /w x86\groove2013-kb3178645-fullfile-x86-glb.exe /quiet /passive
@@ -149,35 +143,44 @@ set /a _num+=1
 echo %_num% Install: conv2013-kb3213564-fullfile-x86-glb.exe
 start /w x86\conv2013-kb3213564-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: onenote2013-kb4011075-fullfile-x86-glb.exe
-start /w x86\onenote2013-kb4011075-fullfile-x86-glb.exe /quiet /passive
-set /a _num+=1
 echo %_num% Install: seguiemj2013-kb4011087-fullfile-x86-glb.exe
 start /w x86\seguiemj2013-kb4011087-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: osfclient2013-kb4011106-fullfile-x86-glb.exe
-start /w x86\osfclient2013-kb4011106-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: ose2013-kb4011253-fullfile-x86-glb.exe
+start /w x86\ose2013-kb4011253-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: oart2013-kb4011277-fullfile-x86-glb.exe
-start /w x86\oart2013-kb4011277-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: osetup2013-kb4011254-fullfile-x86-glb.exe
+start /w x86\osetup2013-kb4011254-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: powerpoint2013-kb4011278-fullfile-x86-glb.exe
-start /w x86\powerpoint2013-kb4011278-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: onenote2013-kb4011281-fullfile-x86-glb.exe
+start /w x86\onenote2013-kb4011281-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: mso2013-kb4011279-fullfile-x86-glb.exe
-start /w x86\mso2013-kb4011279-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: eqnedt322013-kb4011580-fullfile-x86-glb.exe
+start /w x86\eqnedt322013-kb4011580-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: outlook2013-kb4011282-fullfile-x86-glb.exe
-start /w x86\outlook2013-kb4011282-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: oart2013-kb4018288-fullfile-x86-glb.exe
+start /w x86\oart2013-kb4018288-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: lync2013-kb4011284-fullfile-x86-glb.exe
-start /w x86\lync2013-kb4011284-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: powerpoint2013-kb4018289-fullfile-x86-glb.exe
+start /w x86\powerpoint2013-kb4018289-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: project2013-kb4011285-fullfile-x86-glb.exe
-start /w x86\project2013-kb4011285-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: osfclient2013-kb4018333-fullfile-x86-glb.exe
+start /w x86\osfclient2013-kb4018333-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: word2013-kb4011590-fullfile-x86-glb.exe
-start /w x86\word2013-kb4011590-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: outlook2013-kb4018376-fullfile-x86-glb.exe
+start /w x86\outlook2013-kb4018376-fullfile-x86-glb.exe /quiet /passive
 set /a _num+=1
-echo %_num% Install: excel2013-kb4011597-fullfile-x86-glb.exe
-start /w x86\excel2013-kb4011597-fullfile-x86-glb.exe /quiet /passive
+echo %_num% Install: lync2013-kb4018377-fullfile-x86-glb.exe
+start /w x86\lync2013-kb4018377-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: project2013-kb4018379-fullfile-x86-glb.exe
+start /w x86\project2013-kb4018379-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: mso2013-kb4018389-fullfile-x86-glb.exe
+start /w x86\mso2013-kb4018389-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: word2013-kb4018396-fullfile-x86-glb.exe
+start /w x86\word2013-kb4018396-fullfile-x86-glb.exe /quiet /passive
+set /a _num+=1
+echo %_num% Install: excel2013-kb4018399-fullfile-x86-glb.exe
+start /w x86\excel2013-kb4018399-fullfile-x86-glb.exe /quiet /passive
