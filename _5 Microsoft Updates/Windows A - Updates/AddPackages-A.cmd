17d620ed17367db4f4d6c5900d9ea58ad4cf40d0
Andrej Akimov <andrakmv@gmail.com>
2018-05-27 10:13:22 +0300

    Deleted AddPackages-?.cmd scripts

--- _5 Microsoft Updates/Windows A - Updates/AddPackages-A.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Online servicing of Windows 10 image
-echo -------------------------------------------------------------------------------
-echo Adding Netfx updates
-start "" /d Netfx /max /w Netfx-A.cmd
-echo Adding Rollup updates
-start "" /d Rollup /max /w Rollup-A.cmd
