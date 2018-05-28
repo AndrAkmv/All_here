7c3940043266d1b47d2f0039857252fa67d0de18
Andrej Akimov <andrakmv@gmail.com>
2018-05-28 17:22:35 +0300

    New script: EnableNetFx3.cmd

--- _1 Scripts/EnableNetFx3.cmd
+++ _1 Scripts/EnableNetFx3.cmd
@@ -1,19 +1,21 @@
 @echo off
-title Adding Netfx updates into Windows 10 image
+title Enabling NetFx3 in Windows image
 
 set _arch=x64
 set _file=install.wim
 set _img=Online
 set _mnt=mount
 
+ver | find "6.1" > nul && exit
+
 :pre_menu
 cls
-if not exist %_file% goto :check
+if not exist %_file% goto :bit
 dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file%
 echo -------------------------------------------------------------------------------
 if %ERRORLEVEL% NEQ 0 pause & exit
 set /p _ind=Input index or press [Enter] for quit: || exit
-if %_ind% EQU 0 goto :check
+if %_ind% EQU 0 goto :bit
 if %_ind% GTR 0 if %_ind% LEQ 24 goto :ind_menu
 goto :pre_menu
 
@@ -26,15 +28,16 @@ choice /c abcdefghijklmnopqrstuvwxyz /n /m "Mount selected image? [m] "
 if %ERRORLEVEL% EQU 13 goto :mount
 goto :pre_menu
 
-:check
-dism /%_img% /English /LogLevel:1 /Get-Help | find "Image Version: 10" > nul ||^
-if not exist %_file% (exit) else (goto :unmount)
-goto :bit
-
 :bit
 if %_img%==Online (if %PROCESSOR_ARCHITECTURE%==x86 if not defined PROCESSOR_ARCHITEW6432 set _arch=x86)^
 else (dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file% /Index:%_ind% | find "Architecture : x86" > nul && set _arch=x86)
-goto :enable
+goto :version
+
+:version
+dism /%_img% /English /LogLevel:1 /Get-Help | find "Image Version: 6.3" > nul && set _ver=8.1
+dism /%_img% /English /LogLevel:1 /Get-Help | find "Image Version: 10" > nul && set _ver=10
+if defined _ver goto :enable
+goto :unmount
 
 :enable
 cls
@@ -42,18 +45,38 @@ echo Getting list of features. Please wait...
 dism /%_img% /English /LogLevel:1 /Get-Features /Format:Table > %TEMP%\features.txt
 echo -------------------------------------------------------------------------------
 echo Unpacking SXS folder. Please wait...
-start /d %_arch% /w Windows10-SXS-%_arch%.exe
+start /w Windows%_ver%-SXS-%_arch%.exe
 echo -------------------------------------------------------------------------------
 echo Enable: .NET Framework 3.5 (includes .NET 2.0 and 3.0)
 call :state NetFx3 ||^
-dism /%_img% /English /LogLevel:1 /Enable-Feature /FeatureName:NetFx3 /Source:%TEMP%\Windows10-SXS-%_arch% /LimitAccess /NoRestart
+dism /%_img% /English /LogLevel:1 /Enable-Feature /FeatureName:NetFx3 /Source:%TEMP%\Windows%_ver%-SXS-%_arch% /LimitAccess /NoRestart
 echo -------------------------------------------------------------------------------
 del %TEMP%\features.txt
-rd /s /q %TEMP%\Windows10-SXS-%_arch%
-goto :unmount
+rd /s /q %TEMP%\Windows%_ver%-SXS-%_arch%
+goto :add
 
 :state
 findstr %1 %TEMP%\features.txt | find "Enable" > nul
+exit /b
+
+:add
+cls
+echo Getting list of packages. Please wait...
+dism /%_img% /English /LogLevel:1 /Get-Packages > %TEMP%\packages.txt
+echo -------------------------------------------------------------------------------
+for /f "tokens=*" %%i in ('dir %_arch% /b') do call :handle %%i
+del %TEMP%\packages.txt
+goto :unmount
+
+:handle
+set /a _num+=1
+echo %_num% Add: %1
+call :exist %_arch%\%1 || dism /%_img% /English /LogLevel:1 /Add-Package /PackagePath:%_arch%\%1 /NoRestart
+echo -------------------------------------------------------------------------------
+exit /b
+
+:exist
+dism /%_img% /English /LogLevel:1 /Get-PackageInfo /PackagePath:%1 | find "Package Identity" | findstr /g:/ %TEMP%\packages.txt > nul
 exit /b
 
 :mount
@@ -62,10 +85,11 @@ md %_mnt%
 dism /English /LogLevel:1 /Mount-Image /ImageFile:%_file% /Index:%_ind% /MountDir:%_mnt%
 if %ERRORLEVEL% NEQ 0 rd %_mnt% & pause & exit
 set _img=Image:%_mnt%
-goto :check
+goto :bit
 
 :unmount
 cls
+if not exist %_file% exit
 if not %_img%==Online (
 dism /English /LogLevel:1 /Unmount-Image /MountDir:%_mnt% /Commit
 rd %_mnt%
