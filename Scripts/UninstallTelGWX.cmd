db913ac43a3f232439a8bd8c561e07bab8f58657
Andrej Akimov <andrakmv@gmail.com>
2018-12-15 16:59:20 +0200

    Added echo-output for uninstalling update packages

--- Scripts/UninstallTelGWX.cmd
+++ Scripts/UninstallTelGWX.cmd
@@ -1,22 +1,31 @@
 @echo off
-title Uninstal telemetry and GWX updates
 
+echo Attempt to uninstall unwanted update packages
+echo -------------------------------------------------------------------------------
+
+echo KB3150513
 start /w wusa.exe /uninstall /KB:3150513 /quiet /norestart
 
-dism /Online /English /LogLevel:1 /Get-Help | find "Image Version: 6.1" > nul && goto :gwx-7
-dism /Online /English /LogLevel:1 /Get-Help | find "Image Version: 6.3" > nul && goto :gwx-9
+ver | find "6.1" > nul && goto :gwx-7
+ver | find "6.3" > nul && goto :gwx-9
 exit
 
 :gwx-7
+echo KB3021917
 start /w wusa.exe /uninstall /KB:3021917 /quiet /norestart
+echo KB2952664
 start /w wusa.exe /uninstall /KB:2952664 /quiet /norestart
 goto :tel-78
 
 :gwx-9
+echo KB3044374
 start /w wusa.exe /uninstall /KB:3044374 /quiet /norestart
+echo KB2976978
 start /w wusa.exe /uninstall /KB:2976978 /quiet /norestart
 goto :tel-78
 
 :tel-78
+echo KB3080149
 start /w wusa.exe /uninstall /KB:3080149 /quiet /norestart
+echo KB3068708
 start /w wusa.exe /uninstall /KB:3068708 /quiet /norestart
