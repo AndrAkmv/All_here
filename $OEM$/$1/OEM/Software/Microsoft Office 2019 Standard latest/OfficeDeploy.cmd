6e6f2c25aafab387099280913ee88fdb91682b63
Andrej Akimov <andrakmv@gmail.com>
2019-10-18 18:22:32 +0300

    Office 2019 C2R doployment script and config

---
+++ $OEM$/$1/OEM/Software/Microsoft Office 2019 Standard latest/OfficeDeploy.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+ver | find "10" > nul && set _win10=Y
+
+echo Microsoft Office 2019 Standard VL x32
+echo -------------------------------------------------------------------------------
+choice /c abcdefghijklmnopqrstuvwxyz /n /m "Download [d] or Configure [c] Office instalation?"
+if %ERRORLEVEL% EQU 4 start setup.exe /download config.xml
+if %ERRORLEVEL% EQU 3 if defined _win10 start setup.exe /configure config.xml
