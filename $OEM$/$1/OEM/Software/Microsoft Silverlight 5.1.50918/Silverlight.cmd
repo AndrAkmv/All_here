9404a9357aadfba03251ea2f22ae2e201c97374a
Andrej Akimov <andrakmv@gmail.com>
2023-01-15 10:02:10 +0300

    Deleted Silverlight as discontinued project

--- $OEM$/$1/OEM/Software/Microsoft Silverlight 5.1.50918/Silverlight.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set _arch=x64
-if %PROCESSOR_ARCHITECTURE%==x86 if not defined PROCESSOR_ARCHITEW6432 set _arch=x86
-
-echo Microsoft Silverlight 5.1.50918.0
-start /w Silverlight_%_arch%.exe /q
