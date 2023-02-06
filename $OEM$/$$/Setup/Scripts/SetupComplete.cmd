6450e14c1bf4295fc1e7176bcd1d6a78546e6c2b
Andrej Akimov <andrakmv@gmail.com>
2023-02-06 21:51:07 +0300

    Came back separate tweak for Windows 10 Enterprise LTSC

--- $OEM$/$$/Setup/Scripts/SetupComplete.cmd
+++ $OEM$/$$/Setup/Scripts/SetupComplete.cmd
@@ -49,7 +49,7 @@ goto :cleanup
 :windows-C
 start "" /w "%SYSTEMDRIVE%\OEM\Software\Microsoft Office 2019 Standard latest\setup.exe" ^
 /configure "%SYSTEMDRIVE%\OEM\Software\Microsoft Office 2019 Standard latest\config.xml"
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Tweaks /d "cmd /c reg import %SYSTEMROOT%\Setup\Scripts\Tweaks-Y.reg"
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Tweaks /d "cmd /c reg import %SYSTEMROOT%\Setup\Scripts\Tweaks-C.reg"
 goto :cleanup
 
 :windows-X
