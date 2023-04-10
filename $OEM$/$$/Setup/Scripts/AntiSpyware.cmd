8509ca3aae967300403ecc81e58f5d016945a92f
Andrej Akimov <andrakmv@gmail.com>
2023-04-10 08:17:59 +0300

    Opt out of disabling some needed numeric services

--- $OEM$/$$/Setup/Scripts/AntiSpyware.cmd
+++ $OEM$/$$/Setup/Scripts/AntiSpyware.cmd
@@ -7,18 +7,9 @@ reg add %%i /v Start /t REG_DWORD /d 4 /f)                                      
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f BluetoothUserService ^| find "BluetoothUserService"') do (
 reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
 
-for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f CaptureService ^| find "CaptureService"') do (
-reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
-for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f cbdhsvc ^| find "cbdhsvc"') do (
-reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f CDPUserSvc ^| find "CDPUserSvc"') do (
 reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem B C
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f ConsentUxUserSvc ^| find "ConsentUxUserSvc"') do (
-reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
-
-for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f DevicePickerUserSvc ^| find "DevicePickerUserSvc"') do (
-reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
-for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f DevicesFlowUserSvc ^| find "DevicesFlowUserSvc"') do (
 reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem - C
 
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f MessagingService ^| find "MessagingService"') do (
@@ -35,9 +26,6 @@ reg add %%i /v Start /t REG_DWORD /d 4 /f)                                      
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f UnistoreSvc ^| find "UnistoreSvc"') do (
 reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem B C
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f UserDataSvc ^| find "UserDataSvc"') do (
-reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem B C
-
-for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services /k /f WpnUserService ^| find "WpnUserService"') do (
 reg add %%i /v Start /t REG_DWORD /d 4 /f)                                                                                                  &rem B C
 
 timeout 3 > nul
