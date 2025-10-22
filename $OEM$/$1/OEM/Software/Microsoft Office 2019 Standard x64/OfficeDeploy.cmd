b791068b0a83b322a5be8d78e81ffbfc0d1ce207
Andrej Akimov <andrakmv@gmail.com>
2025-10-22 12:42:32 +0300

    Repaired Office 2019 & 2021 deployment scripts and configs

--- $OEM$/$1/OEM/Software/Microsoft Office 2019 Standard x64/OfficeDeploy.cmd
+++ $OEM$/$1/OEM/Software/Microsoft Office 2019 Standard x64/OfficeDeploy.cmd
@@ -4,6 +4,6 @@ ver | find "10" > nul && set _win10=Y
 
 echo Microsoft Office 2019 Standard x64
 echo -------------------------------------------------------------------------------
-choice /c abcdefghijklmnopqrstuvwxyz /n /m "Download [d] or Configure [c] Office instalation?"
-if %ERRORLEVEL% EQU 4 start setup.exe /download config.xml
-if %ERRORLEVEL% EQU 3 if defined _win10 start setup.exe /configure config.xml
+choice /c cde /n /m "Download [d] or Configure [c] Office instalation? Press [e] to Exit."
+if %ERRORLEVEL% EQU 1 if defined _win10 start setup.exe /configure config.xml
+if %ERRORLEVEL% EQU 2 start setup.exe /download config.xml
--- $OEM$/$1/OEM/Software/Microsoft Office 2021 Standard x64/OfficeDeploy.cmd
+++ $OEM$/$1/OEM/Software/Microsoft Office 2021 Standard x64/OfficeDeploy.cmd
@@ -4,6 +4,9 @@ ver | find "10" > nul && set _win10=Y
 
 echo Microsoft Office 2021 Standard x64
 echo -------------------------------------------------------------------------------
-choice /c abcdefghijklmnopqrstuvwxyz /n /m "Download [d] or Configure [c] Office instalation?"
-if %ERRORLEVEL% EQU 4 start setup.exe /download config.xml
-if %ERRORLEVEL% EQU 3 if defined _win10 start setup.exe /configure config.xml
+choice /c cde /n /m "Download [d] or Configure [c] Office instalation? Press [e] to Exit."
+if %ERRORLEVEL% EQU 1 if defined _win10 start setup.exe /configure config.xml & exit
+if %ERRORLEVEL% EQU 3 exit
+reg add HKCU\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\Ecs /v CountryCode /d "std::wstring|US" /f > nul
+start setup.exe /download config.xml
+reg add HKCU\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\Ecs /v CountryCode /d "std::wstring|RU" /f > nul
