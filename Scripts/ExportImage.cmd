8ea7298c226162f9cf6a8ccef3ce416818cdc407
Andrej Akimov <andrakmv@gmail.com>
2025-10-26 09:42:23 +0300

    Revised script for export Windows image to another file

--- Scripts/ExportImage.cmd
+++ Scripts/ExportImage.cmd
@@ -2,36 +2,26 @@
 title Exporting Windows image to another file (optimize)
 
 set _file=install.wim
+set _type=maximum
+set _ext=wim
 
 dism /English /LogLevel:1 /Get-Help | find "Version: 6.1" > nul && exit
 
-:pre_menu
-cls
 if not exist %_file% exit
 dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file%
 echo -------------------------------------------------------------------------------
 if %ERRORLEVEL% NEQ 0 pause & exit
 set /p _ind=Input index or press [Enter] for quit: || exit
-if %_ind% GTR 0 if %_ind% LEQ 24 goto :exp_menu
-goto :pre_menu
 
-:exp_menu
 cls
 dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file% /Index:%_ind%
 echo -------------------------------------------------------------------------------
-if %ERRORLEVEL% NEQ 0 pause & goto :pre_menu
+if %ERRORLEVEL% NEQ 0 pause & exit
 choice /c ew /n /m "Export selected image to .esd [e] or to .wim [w]? "
-if %ERRORLEVEL% EQU 1 goto :recovery
-goto :maximum
+if %ERRORLEVEL% EQU 1 set _type=recovery & set _ext=esd
 
-:recovery
+:export
 cls
-if exist install%_num%.esd set /a _num+=1 & goto :recovery
-dism /English /LogLevel:1 /Export-Image /SourceImageFile:%_file% /SourceIndex:%_ind% /DestinationImageFile:install%_num%.esd /Compress:recovery
-goto :pre_menu
-
-:maximum
-cls
-if exist install%_num%.wim set /a _num+=1 & goto :maximum
-dism /English /LogLevel:1 /Export-Image /SourceImageFile:%_file% /SourceIndex:%_ind% /DestinationImageFile:install%_num%.wim /Compress:maximum
-goto :pre_menu
+if exist install%_num%.wim set /a _num+=1 & goto :export
+dism /English /LogLevel:1 /Export-Image /SourceImageFile:%_file% /SourceIndex:%_ind% /DestinationImageFile:install%_num%.%_ext% /Compress:%_type%
+if %ERRORLEVEL% NEQ 0 pause
