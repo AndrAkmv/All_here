200fccf94ba036755c4cb6c2a50f264f296f13c4
Andrej Akimov <andrakmv@gmail.com>
2025-11-30 15:56:32 +0300

    Deleted deprecated update script and security restore parameters

--- Scripts/ReceiveUpdates.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-powershell -ExecutionPolicy ByPass -Command "$ms = New-Object -ComObject Microsoft.Update.ServiceManager; $ms.AddService2('7971f918-a847-4430-9279-4a52d1efe18d',7,'')"
-timeout 3 > nul
