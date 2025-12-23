a777d33b8caf2e748b8690278e720c3712e4539d
Andrej Akimov <andrakmv@gmail.com>
2025-12-23 23:11:42 +0300

    Removed architecture detection from .NET Framework setup script

--- $OEM$/$1/OEM/Software/Microsoft .NET Framework 4.8/Net_Framework.cmd
+++ $OEM$/$1/OEM/Software/Microsoft .NET Framework 4.8/Net_Framework.cmd
@@ -2,9 +2,6 @@
 
 ver | find "6.1" > nul || goto exit
 
-set _arch=x64
-if %PROCESSOR_ARCHITECTURE%==x86 if not defined PROCESSOR_ARCHITEW6432 set _arch=x86
-
 echo Microsoft .NET Framework Russian
 echo -------------------------------------------------------------------------------
 echo .NET Framework 4.8
@@ -12,4 +9,4 @@ start /w ndp48-x86-x64-allos-enu.exe /passive /norestart
 echo Russian Language Pack
 start /w ndp48-x86-x64-allos-rus.exe /passive /norestart
 echo Rollup updates (Dec 2019)
-start /w ndp48-kb4532941-%_arch%.exe /passive /norestart
+start /w ndp48-kb4532941-x64.exe /passive /norestart
