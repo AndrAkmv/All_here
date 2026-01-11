967e34c7ce859d13a2e2c946d22101fddbe77abc
Andrej Akimov <andrakmv@gmail.com>
2026-01-11 09:11:43 +0300

    Revised script for removing appxes from Windows image

--- Scripts/RemoveAppxes.cmd
+++ Scripts/RemoveAppxes.cmd
@@ -1,36 +1,36 @@
 @echo off
-title Deleting Appxes in Windows image
+title Removing appxes from Windows image
 
 set _file=install.wim
+set _make=Commit
 set _img=Online
 set _mnt=mount
 
 dism /English /LogLevel:1 /Get-Help | find "Version: 6.1" > nul && exit
 
-:pre_menu
-cls
 if not exist %_file% goto :remove
 dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file%
 echo -------------------------------------------------------------------------------
 if %ERRORLEVEL% NEQ 0 pause & exit
 set /p _ind=Input index or press [Enter] for quit: || exit
-if %_ind% EQU 0 goto :remove
-if %_ind% GTR 0 if %_ind% LEQ 24 goto :ind_menu
-goto :pre_menu
 
-:ind_menu
 cls
 dism /English /LogLevel:1 /Get-ImageInfo /ImageFile:%_file% /Index:%_ind%
 echo -------------------------------------------------------------------------------
-if %ERRORLEVEL% NEQ 0 pause & goto :pre_menu
+if %ERRORLEVEL% NEQ 0 pause & exit
 choice /c abcdefghijklmnopqrstuvwxyz /n /m "Mount selected image? [m] "
-if %ERRORLEVEL% EQU 13 goto :mount
-goto :pre_menu
+if %ERRORLEVEL% NEQ 13 exit
+
+cls
+md %_mnt%
+dism /English /LogLevel:1 /Mount-Image /ImageFile:%_file% /Index:%_ind% /MountDir:%_mnt%
+if %ERRORLEVEL% NEQ 0 rd %_mnt% & pause & exit
+set _img=Image:%_mnt%
 
 :remove
 cls
 echo Getting list of appxes. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-ProvisionedAppxPackages > %TEMP%\appxes.txt
+dism /English /LogLevel:1 /%_img% /Get-ProvisionedAppxPackages > %TEMP%\appxes.txt
 echo -------------------------------------------------------------------------------
 for /f "skip=8 tokens=3" %%i in (%TEMP%\appxes.txt) do call :filter %%i
 del %TEMP%\appxes.txt
@@ -38,7 +38,6 @@ if %_img%==Online (
 echo Remove current Appxes for AllUsers
 powershell -ExecutionPolicy Bypass -Command "Get-AppxPackage | Remove-AppxPackage"
 )
-if not exist %_file% exit
 goto :unmount
 
 :filter
@@ -48,24 +47,14 @@ exit /b
 :action
 set /a _num+=1
 echo %_num% Remove: %1
-dism /%_img% /English /LogLevel:1 /Remove-ProvisionedAppxPackage /PackageName:%1
+dism /English /LogLevel:1 /%_img% /Remove-ProvisionedAppxPackage /PackageName:%1
 echo -------------------------------------------------------------------------------
+if %ERRORLEVEL% NEQ 0 if %ERRORLEVEL% NEQ 3010 set _make=Discard & pause
 exit /b
-
-:mount
-cls
-md %_mnt%
-dism /English /LogLevel:1 /Mount-Image /ImageFile:%_file% /Index:%_ind% /MountDir:%_mnt%
-if %ERRORLEVEL% NEQ 0 rd %_mnt% & pause & exit
-set _img=Image:%_mnt%
-goto :remove
 
 :unmount
 cls
 if not exist %_file% exit
-if not %_img%==Online (
-dism /English /LogLevel:1 /Unmount-Image /MountDir:%_mnt% /Commit
+dism /English /LogLevel:1 /Unmount-Image /MountDir:%_mnt% /%_make%
+if %ERRORLEVEL% NEQ 0 pause
 rd %_mnt%
-)
-set _img=Online
-goto :pre_menu
