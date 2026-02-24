8798d0c0aaa58b9c6968ac82212e4273897b1916
Andrej Akimov <andrakmv@gmail.com>
2026-02-24 16:23:28 +0300

    Revert "Added script for enable backup and ability to boot legacy menu"
    This reverts commit 939f7723dbcb376d095632731ef8080d9d994193.

--- Scripts/LegacyBoot.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-ver | find "6.1" > nul && exit
-
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Configuration Manager\LastKnownGood" /v Enabled /t REG_DWORD /d 1 /f
-bcdedit /set {current} bootmenupolicy legacy
-timeout 3 > nul
