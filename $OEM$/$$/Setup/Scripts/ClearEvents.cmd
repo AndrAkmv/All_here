290960a737a893a55e1d51314ace0b607e3414e1
Andrej Akimov <andrakmv@gmail.com>
2026-05-27 17:39:47 +0300

    Updated script for clearing log events after installation

--- $OEM$/$$/Setup/Scripts/ClearEvents.cmd
+++ $OEM$/$$/Setup/Scripts/ClearEvents.cmd
@@ -1,16 +1,15 @@
 @echo off
 
-ver | find "10" > nul || exit
+wevtutil cl Microsoft-Windows-PrintService/Admin                                                                                                 &rem - - B C -
+wevtutil cl Microsoft-Windows-SMBWitnessClient/Admin                                                                                             &rem - - B C -
+wevtutil cl Microsoft-Windows-Kernel-EventTracing/Admin                                                                                          &rem - - B - D
+wevtutil cl Microsoft-Windows-Provisioning-Diagnostics-Provider/AutoPilot                                                                        &rem - - - C D
+wevtutil cl Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Admin                                                             &rem - - B - D
 
-wevtutil cl Application                                                                                                                              &rem B ? D
-wevtutil cl Microsoft-Windows-AppReadiness/Admin                                                                                                     &rem - ? D
-wevtutil cl Microsoft-Windows-PrintService/Admin                                                                                                     &rem B ? ?
-wevtutil cl Microsoft-Windows-SMBWitnessClient/Admin                                                                                                 &rem B ? -
-wevtutil cl Microsoft-Windows-DeviceSetupManager/Admin                                                                                               &rem B ? ?
-wevtutil cl Microsoft-Windows-Kernel-EventTracing/Admin                                                                                              &rem B ? -
-wevtutil cl Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Admin                                                                 &rem B ? D
-
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v AppMod /d "cmd /c wevtutil cl Microsoft-Windows-AppModel-Runtime/Admin"            &rem B ? D
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v System /d "cmd /c wevtutil cl System"                                              &rem B ? D
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Device /d "cmd /c wevtutil cl Microsoft-Windows-DeviceSetupManager/Admin"      &rem - 9 B C D
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Modela /d "cmd /c wevtutil cl Microsoft-Windows-AppModel-Runtime/Admin"        &rem - - B C D
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Readin /d "cmd /c wevtutil cl Microsoft-Windows-AppReadiness/Admin"            &rem - 9 - C D
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Applic /d "cmd /c wevtutil cl Application"                                     &rem 7 9 B C D
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v System /d "cmd /c wevtutil cl System"                                          &rem 7 9 B C D
 
 timeout 3 > nul
