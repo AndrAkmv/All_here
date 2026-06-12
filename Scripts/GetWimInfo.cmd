efa728c09640ead48e54f026eb12f4baa71721c5
Andrej Akimov <andrakmv@gmail.com>
2026-06-12 19:29:09 +0300

    Changed sequence of input parameters for DISM commands

--- Scripts/GetWimInfo.cmd
+++ Scripts/GetWimInfo.cmd
@@ -62,23 +62,23 @@ goto :unmount
 
 :editions
 cls
-dism /%_img% /English /LogLevel:1 /Get-CurrentEdition
+dism /English /LogLevel:1 /%_img% /Get-CurrentEdition
 echo -------------------------------------------------------------------------------
-dism /%_img% /English /LogLevel:1 /Get-TargetEditions
+dism /English /LogLevel:1 /%_img% /Get-TargetEditions
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
 
 :languages
 cls
-dism /%_img% /English /LogLevel:1 /Get-Intl
+dism /English /LogLevel:1 /%_img% /Get-Intl
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
 
 :drivers
 cls
-dism /%_img% /English /LogLevel:1 /Get-Drivers /All | more
+dism /English /LogLevel:1 /%_img% /Get-Drivers /All | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -87,12 +87,12 @@ goto :adv_menu
 cls
 if exist drivers%_numd%.txt set /a _numd+=1 & goto :drivers-export
 echo Getting list of drivers. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-Drivers /All /Format:Table > drivers%_numd%.txt
+dism /English /LogLevel:1 /%_img% /Get-Drivers /All /Format:Table > drivers%_numd%.txt
 goto :adv_menu
 
 :packages
 cls
-dism /%_img% /English /LogLevel:1 /Get-Packages | more
+dism /English /LogLevel:1 /%_img% /Get-Packages | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -101,12 +101,12 @@ goto :adv_menu
 cls
 if exist packages%_nump%.txt set /a _nump+=1 & goto :packages-export
 echo Getting list of packages. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-Packages /Format:Table > packages%_nump%.txt
+dism /English /LogLevel:1 /%_img% /Get-Packages /Format:Table > packages%_nump%.txt
 goto :adv_menu
 
 :features
 cls
-dism /%_img% /English /LogLevel:1 /Get-Features | more
+dism /English /LogLevel:1 /%_img% /Get-Features | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -115,12 +115,12 @@ goto :adv_menu
 cls
 if exist features%_numf%.txt set /a _numf+=1 & goto :features-export
 echo Getting list of features. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-Features /Format:Table > features%_numf%.txt
+dism /English /LogLevel:1 /%_img% /Get-Features /Format:Table > features%_numf%.txt
 goto :adv_menu
 
 :capabils
 cls
-dism /%_img% /English /LogLevel:1 /Get-Capabilities /LimitAccess | more
+dism /English /LogLevel:1 /%_img% /Get-Capabilities /LimitAccess | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -129,12 +129,12 @@ goto :adv_menu
 cls
 if exist capabils%_numc%.txt set /a _numc+=1 & goto :capabils-export
 echo Getting list of capabils. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-Capabilities /LimitAccess /Format:Table > capabils%_numc%.txt
+dism /English /LogLevel:1 /%_img% /Get-Capabilities /LimitAccess /Format:Table > capabils%_numc%.txt
 goto :adv_menu
 
 :associat
 cls
-dism /%_img% /English /LogLevel:1 /Get-DefaultAppAssociations | more
+dism /English /LogLevel:1 /%_img% /Get-DefaultAppAssociations | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -143,12 +143,12 @@ goto :adv_menu
 cls
 if exist associat%_nums%.txt set /a _nums+=1 & goto :associat-export
 echo Getting list of associat. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-DefaultAppAssociations > associat%_nums%.txt
+dism /English /LogLevel:1 /%_img% /Get-DefaultAppAssociations > associat%_nums%.txt
 goto :adv_menu
 
 :appxes
 cls
-dism /%_img% /English /LogLevel:1 /Get-ProvisionedAppxPackages | more
+dism /English /LogLevel:1 /%_img% /Get-ProvisionedAppxPackages | more
 echo -------------------------------------------------------------------------------
 choice /c b /n /m "Back [b] "
 goto :adv_menu
@@ -157,7 +157,7 @@ goto :adv_menu
 cls
 if exist appxes%_numx%.txt set /a _numx+=1 & goto :appxes-export
 echo Getting list of appxes. Please wait...
-dism /%_img% /English /LogLevel:1 /Get-ProvisionedAppxPackages > appxes%_numx%.txt
+dism /English /LogLevel:1 /%_img% /Get-ProvisionedAppxPackages > appxes%_numx%.txt
 goto :adv_menu
 
 :unmount
