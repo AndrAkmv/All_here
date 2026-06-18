5c9f41885cfb00b8f8e9421953ca335a5a8ce5a0
Andrej Akimov <andrakmv@gmail.com>
2026-06-18 20:03:51 +0300

    Disabled autologger diagtrack listener after reboot

--- $OEM$/$$/Setup/Scripts/NoAnimations.cmd
+++ $OEM$/$$/Setup/Scripts/NoAnimations.cmd
@@ -15,4 +15,12 @@ reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v Tskbar /d ^
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v UsrMsk /d ^
 "cmd /c reg add \"HKCU\Control Panel\Desktop\" /v UserPreferencesMask /t REG_BINARY /d 9012038012000000 /f"
 
+ver | find "10.0.19044" > nul
+if %ERRORLEVEL% NEQ 0 (
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v WMILog /d ^
+"cmd /c reg add \"HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\AutoLogger-Diagtrack-Listener\" /v Start /t REG_DWORD /d 0 /f"
+) else (
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v WMILog /d ^
+"cmd /c reg add \"HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\Diagtrack-Listener\" /v Start /t REG_DWORD /d 0 /f"
+)
 timeout 3 > nul
